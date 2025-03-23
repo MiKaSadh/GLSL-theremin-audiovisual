bfb1a4ec74cec5c0d0959457653a369859f83e07
mikesasu <misakatakahisha1994@icloud.com>
2025-03-23 20:29:07 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let shaderProgram;
+let audio;
+
+function preload() {
+    shaderProgram = loadShader('shader.vert', 'shader.frag');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    noStroke();
+    audio = new Theremin();  // オーディオクラスのインスタンス
+}
+
+function draw() {
+    shader(shaderProgram);
+    shaderProgram.setUniform('time', millis() / 1000.0);
+    shaderProgram.setUniform('freq', audio.getFrequency()); 
+    shaderProgram.setUniform('amp', audio.getAmplitude());
+    rect(0, 0, width, height);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
